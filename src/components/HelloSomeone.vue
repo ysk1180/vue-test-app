b1e9cbb32de8ea1c042974ef676b6e8c2e80d37a
yusuke_miyazaki <ysk1180@gmail.com>
2020-12-11 14:54:51 +0900

    add Hello donghee

---
+++ src/components/HelloSomeone.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Hello! {{ name }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HelloSomeone',
+  props: {
+    name: String,
+  },
+});
+</script>
---
+++ src/views/Hello.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <HelloSomeone name="Donghee"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HelloSomeone from '@/components/HelloSomeone.vue';
+
+export default defineComponent({
+  name: 'Hello',
+  components: {
+    HelloSomeone,
+  },
+});
+</script>
